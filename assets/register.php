eaae140c6f661feb3086f85fbcae32df419c44ca
Glenn Vinck <vinck_glenn@hotmail.com>
2023-04-20 14:12:01 +0200

    feat: setup the project & build registration form

--- assets/register.php
+++ assets/register.php
@@ -1,12 +1,55 @@
-<!DOCTYPE html>
+<?php
+  if(!empty($_POST)){
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $options = [
+      'cost' => 12,
+    ];
+
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT, $options);
+
+    $conn = new mysqli("ID394672_eindbaas.db.webhosting.be", "ID394672_eindbaas", "Eindbaas123", "ID394672_eindbaas");
+    $result = $conn->query("insert into users (username, password) values ('".$conn->real_escape_string($username)."', '".$conn->real_escape_string($password)."')");
+}
+
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PrompTopia: Register</title>
+<link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
-    
+	<div class="PromptopiaRegister">
+		<div class="form form--login">
+			<form action="" method="post">
+				<h2 form__title>Register</h2>
+
+				<?php if( isset($error) ):?>
+					<div class="form__error">
+						<p>
+							Sorry, we can't log you in with that email address and password. Can you try again?
+						</p>
+					</div>
+				<?php endif; ?>
+
+				<div class="form__field">
+					<label for="Email">Email</label>
+					<input type="text" id="username" name="username">
+				</div>
+				<div class="form__field">
+					<label for="Password">Password</label>
+					<input type="password" id="password" name="password">
+				</div>
+
+				<div class="form__field">
+					<input type="submit" value="Sign Up" class="btn btn--primary">	
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,13 +1,31 @@
+<?php 
+
+$conn = new mysqli("ID394672_eindbaas.db.webhosting.be", "ID394672_eindbaas", "Eindbaas123", "ID394672_eindbaas");
+$result = $conn->query("SELECT * FROM users");
+
+var_dump($result);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style.css">
+    <title>PrompTopia</title>
+<link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+<div class="topnav">
+		<a href="#">Logo</a>
+		<form>
+			<input type="text" placeholder="Search...">
+			<button type="submit">Search</button>
+		</form>
+        <a href="#">Marketplace</a>
+		<a href="#">Login</a>
+		<a href="#">Contact</a>
+    </div>
 </body>
 </html>
