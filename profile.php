054c985559853480f4878004b7c0bbd1718a2685
Wolf <wolfverelst@gmail.com>
2023-05-28 10:50:21 +0200

    feat: my liked prompts on my profile

--- profile.php
+++ profile.php
@@ -19,8 +19,8 @@ $profilePicture = $user->getProfilePicture();
 // Prompts 
 $prompts = \PrompTopia\Framework\Prompt::getAllFromUser($_SESSION['username']);
 
-
-
+// Liked Prompts
+$likedPrompts = \PrompTopia\Framework\Like::getLikedPromptsByUser($_SESSION['username']);
 
 // Biography
 $user = new \PrompTopia\Framework\User();
@@ -85,6 +85,26 @@ $biography = $user->getBiography();
     <?php endif; ?>
 </div>
 <!-- End of Prompts -->
+
+
+<!-- Liked Prompts -->
+<h1 style="margin: 60px; text-align: center;">Liked Prompts</h1>
+<div class="prompts">
+    <?php if (count($likedPrompts) > 0): ?>
+        <?php foreach($likedPrompts as $prompt): ?>
+            <div class="prompt-card">
+                <?php include "assets/promptcard.php"; ?>
+                <form action="remove_prompt.php" method="post" class="remove-prompt-form">
+                    <input type="hidden" name="prompt_id" value="<?php echo $prompt['id']; ?>">
+                    <button type="button" class="remove-prompt-button" onclick="confirmPromptRemoval(this)">Remove</button>
+                </form>
+            </div>
+        <?php endforeach; ?>
+    <?php else: ?>
+        <p>No liked prompts found here..</p>
+    <?php endif; ?>
+</div>
+<!-- End of Liked Prompts -->
 
 <script>
     function confirmPromptRemoval(button) {
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '6cbdfc0fa335dcc235c1829ed213c38818611d49',
+        'reference' => '3ae26ead603b804270623e0a10cf08b07062a5bd',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '6cbdfc0fa335dcc235c1829ed213c38818611d49',
+            'reference' => '3ae26ead603b804270623e0a10cf08b07062a5bd',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
